8f224b14b5ca3ec66cf81b15771776a2b87387c9
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-11-08 18:01:42 -0500

    Basic pages views, templates, and an example

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flaskext.flatpages import FlatPages
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+pages = FlatPages(app)
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    template = page.meta.get('template', 'flatpage.html')
+    return render_template(template, page=page)
 
 if __name__ == "__main__":
     app.run()
